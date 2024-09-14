d9c3fd0777053dc80c293b7aec17a8277857f4ff
1ShaikhAsif <142223292+1ShaikhAsif@users.noreply.github.com>
2024-09-14 14:27:29 +0530

    Create mongodb_backups.sh

---
+++ mongodb_backups.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+MONGO_URI="your mongodb uri"
+BACKUP_PATH="/home/ubuntu/daily-bd-backups"
+DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+BACKUP_NAME="mongo_backup_$DATE"
+BACKUP_FILE="$BACKUP_PATH/$BACKUP_NAME.gz"
+
+
+mkdir -p $BACKUP_PATH
+
+
+mongodump --uri="$MONGO_URI" --archive=$BACKUP_FILE --gzip
+
+# Check if the backup was successful
+if [ $? -eq 0 ]; then
+    echo "Backup successful: $BACKUP_FILE"
+else
+    echo "Backup failed!"
+    exit 1
+fi
+
+
+find $BACKUP_PATH -type f -mtime +7 -name "*.gz" -exec rm {} \;
+
+
